628b8cf6c34e5dda827c5e737aedfe120e78d837
Leonardo <leofelix1983@gmail.com>
2026-07-22 14:17:02 -0300

    modificação no contato

--- teste-cypress/cypress/e2e/contato.cy.js
+++ teste-cypress/cypress/e2e/contato.cy.js
@@ -11,4 +11,40 @@ describe('funcionalidade: contato', () => {
   cy.get('#btn-submit').click()
   cy.contains("Contato enviado com sucesso").should("exist")
   })
+
+  it('deve validar erro ao enviar sem preencher o nome', () => {
+  //cy.get('[name="name"]').type("leo")
+  cy.get('[name="email"]').type("teste@teste.com")
+  cy.get('[name="subject"]').select("Sugestões")
+  cy.get('[name="message"]').type("qualquer mensagem")
+  cy.get('#btn-submit').click()
+  cy.get('#alert-container').should("contain", "Por favor, preencha o campo Nome")
+  })
+
+  it('deve validar erro ao enviar sem preencher o e-mail', () => {
+  cy.get('[name="name"]').type("leo")
+  //cy.get('[name="email"]').type("teste@teste.com")
+  cy.get('[name="subject"]').select("Sugestões")
+  cy.get('[name="message"]').type("qualquer mensagem")
+  cy.get('#btn-submit').click()
+  cy.get('#alert-container').should("contain", "Por favor, preencha o campo E-mail")
+  });
+
+  it('deve validar erro ao enviar sem selecionar o assunto', () => {
+  cy.get('[name="name"]').type("leo")
+  cy.get('[name="email"]').type("teste@teste.com")
+  //cy.get('[name="subject"]').select("Sugestões")
+  cy.get('[name="message"]').type("qualquer mensagem")
+  cy.get('#btn-submit').click()
+  cy.get('#alert-container').should("contain", "Por favor, selecione o Assunto")
+  });
+
+  it('deve validar erro ao enviar sem escrever a mensagem', () => {
+  cy.get('[name="name"]').type("leo")
+  cy.get('[name="email"]').type("teste@teste.com")
+  cy.get('[name="subject"]').select("Sugestões")
+  //cy.get('[name="message"]').type("qualquer mensagem")
+  cy.get('#btn-submit').click()
+  cy.get('#alert-container').should("contain", "Por favor, escreva sua Mensagem")
+  });
 })
